325a135aeeba58612987fc53c066d192c798d296
dunglk85 <dunglk85@gmail.com>
2025-02-09 12:10:17 +0700

    custom template tag

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from blog.models import Post
 
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now()) # This will get all the posts from the database
-    return render(request, 'blog/index.html', {'post': posts}) # This will render the index.html template
+    return render(request, 'blog/index.html', {'posts': posts}) # This will render the index.html template
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post":post})
