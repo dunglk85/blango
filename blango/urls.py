cfd0700e3a5e41962188621e23e704df7619e911
dunglk85 <dunglk85@gmail.com>
2025-02-12 08:41:42 +0700

    DRF view

--- blango/urls.py
+++ blango/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("accounts/register/", RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
 ]
