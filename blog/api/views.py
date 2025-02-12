f15ea5ce36dca60c59196f269ed0f341dfbbdb68
dunglk85 <dunglk85@gmail.com>
2025-02-12 22:45:24 +0700

    nest relationships

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 
 from blango_auth.models import User
-from blog.api.serializers import PostSerializer, UserSerializer
+from blog.api.serializers import PostDetailSerializer, PostSerializer, UserSerializer
 from blog.models import Post
 from rest_framework.authentication import SessionAuthentication
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
@@ -16,9 +16,8 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostDetailSerializer
     
 class UserDetail(generics.RetrieveAPIView):
-    lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
